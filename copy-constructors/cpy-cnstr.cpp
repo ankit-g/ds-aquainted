fc5b3de0d713237b93407a8077c67335c1749a6b
ankit-g <ankitgupta.work@gmail.com>
2017-03-25 02:28:20 +0530

    yesterdays work who cares

--- copy-constructors/cpy-cnstr.cpp
+++ copy-constructors/cpy-cnstr.cpp
@@ -8,11 +8,46 @@
  *	The solution to this problem is deep copy. Hurrayyyy !!!
  *	In case of deep copy we can use refence to allocate and have seperate space for pointers
  *	and voila the problem disappears.
+ *
+ *	Topics:
+ *
+ *1	C vs C++				|	<done>
+ *2	Reference Variables			|
+ *3	Function Overloading			|
+ *4	Default Argument			|
+ *5	Inline Functions			|
+ *6	new and delete				|
+ *7	Class and Object			|
+ *8	Static Members				|
+ *9	‘this’ Pointer				|
+ *10	Constructor and Destructor		|	<done> pointers can't be used in <deep> copy constructors. only use refence
+ *11	Inheritance				|
+ *12	Friend					|
+ *13	Virtual Functions			|
+ *14	Operator Overloading			|
+ *15	Exception Handling			|
+ *16	Templates				|
+ *17	Namespaces				|	<done>
+ *18	Standard Template Library (STL)		|
+ *19	Misc					|
+ *20	C++ vs Java				|
+ *21	Puzzles					|
+ *22	Output Questions			|
+ *23	Quizzes on C++				|
  */
 
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+struct MyApp
+{
+	int var_a;
+	int var_b;
+	struct MyApp &ref;
+	MyApp() :ref(*this){}
+};
+
 
 struct Test
 {
@@ -50,16 +85,43 @@ public:
 	int var_b; 
 };
 
+namespace kopal
+{
+	int cout = 77;
+	namespace inner_kopal
+	{
+		int cout = 456;
+	}
+}
+
+int cout = 99;
+
 int main(void)
 {
+	const int ajr = 0;
+	int cout = 111;
+	int a = 99;
+	int& b = a;
+
+	MyApp mapp;
+
+	mapp.var_a = 100;
+
+	std::cout <<  mapp.var_a << endl;
+
+	std::cout << b << endl;
 	MyClass *t_obj = new MyClass();
-	cout << t_obj->ptr->a << endl;
+	std::cout << t_obj->ptr->a << endl;
 	{
 		MyClass My_obj(*t_obj);
-		cout << My_obj.ptr->a << endl;
+		std::cout << My_obj.ptr->a << endl;
 	}
 	
-	cout << t_obj->ptr->a << endl;
+	std::cout << t_obj->ptr->a << endl;
+
+	std::cout << kopal::cout << " " << cout << " " << ::cout << endl;
+
+	std::cout << kopal::inner_kopal::cout << endl;
 
 	return EXIT_SUCCESS;
 }
